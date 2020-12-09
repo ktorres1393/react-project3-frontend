6218850d1efce532bb20a3d8e96b293ea3454510
Kassandra Torres <ktorres1393@gmail.com>
2020-12-09 10:55:29 -0500

    Finishes signup feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import { useState } from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+
 import Dashboard from './pages/Dashboard';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage'
+import { getUser } from './services/userService'
+
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 
-function App() {
+
+function App(props) {
+
+  /**  Component State   */
+
+  const [ userState, setUserState ] = useState({ user: getUser() });
+
+  /*   helper functions   */
+
+  function handleSignupOrLogin (){
+    setUserState({ user: getUser()})
+    props.history.push('/dashboard');
+  }
+
   return (
     <div className="App">
      
       <Header />
-      <Dashboard />
+      <Switch>
+      <Route exact path="/" render={(props) =>
       <HomePage />
-      <LoginPage />
-      <SignupPage />
+      }/>
+      <Route exact path="/dashboard" render={(props) =>
+      <Dashboard />
+      }/>
+      <Route exact path="/login" render={(props) =>
+      <LoginPage handleSignupOrLogin={handleSignupOrLogin} />
+      }/>
+      <Route exact path="/signup" render={(props) =>
+      <SignupPage handleSignupOrLogin={handleSignupOrLogin} />
+      }/>
+      
+      </Switch>
       <Footer />
         
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,19 @@
+import SetTimer from '../components/SetTimer/SetTimer'
+import React from 'react';
+
+
 const HomePage = (props)=> (
     
     <main className="Page">
         <h1>HomePage</h1>
+        <div className="flex">
+        <SetTimer />
+        <SetTimer />
+        </div>
+        <div>
+            Clock here
+        </div>
+        
     </main>
   
   )
