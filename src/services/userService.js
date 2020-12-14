e4787b49187f8e366ac64193aed94cfb0d2f9437
Kassandra Torres <ktorres1393@gmail.com>
2020-12-13 23:26:53 -0500

    Deploys to heroku

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,5 @@
 import { getUserFromToken, setToken, removeToken } from './tokenService'
-const BASE_URL = 'http://localhost:3001/api/users'
+const BASE_URL = 'https://study-buddy-backend-curiosity.herokuapp.com/api/users'
 
 function signup(user) {
     return fetch(BASE_URL + '/signup', {
